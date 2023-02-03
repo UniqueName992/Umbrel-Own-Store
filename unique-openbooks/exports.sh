ab4cdc1811a8aa18cf2ab4dc3027b0de7a657860
Your Name <you@example.com>
2023-02-03 23:17:52 +0000

    New name gen script

--- unique-openbooks/exports.sh
+++ unique-openbooks/exports.sh
@@ -1,12 +1,4 @@
-which jq &> /dev/null
-if [[ $? -ne 0 ]]
-then
-    echo "Please install jq first, see https://stedolan.github.io/jq/"
-    exit 1
-fi
-name=$(curl -s https://randomuser.me/api/)
-
-first=$(echo $name | jq .results[0].name | jq .first | sed 's/"//g')
-last=$(echo $name | jq .results[0].name | jq .last | sed 's/"//g')
-
-export RM_NAME="${first^} ${last^}"
+export RM_NAME_name=$(curl -s https://randomuser.me/api/)
+export RM_NAME_first=$(echo $RM_NAME_name | jq ."results[0]".name.first | sed 's/"//g')
+export RM_NAME_last=$(echo $RM_NAME_name | jq ."results[0]".name.last | sed 's/"//g')
+export RM_NAME="$RM_NAME_first $RM_NAME_last"
