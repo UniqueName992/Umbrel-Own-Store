8e4d99639744da89cbfc8e730a9286ec477bdffd
Your Name <you@example.com>
2023-02-03 23:23:32 +0000

    names change

--- unique-openbooks/exports.sh
+++ unique-openbooks/exports.sh
@@ -1,4 +1,4 @@
 export RM_NAME_name=$(curl -s https://randomuser.me/api/)
 export RM_NAME_first=$(echo $RM_NAME_name | jq ."results[0]".name.first | sed 's/"//g')
 export RM_NAME_last=$(echo $RM_NAME_name | jq ."results[0]".name.last | sed 's/"//g')
-export RM_NAME="$RM_NAME_first $RM_NAME_last"
+export RM_NAME="$RM_NAME_first"_"$RM_NAME_last"
