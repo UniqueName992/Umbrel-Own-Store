736ab026d147c74e055e80dd1c76ab1fdef76ce2
Your Name <you@example.com>
2023-02-05 19:10:45 +0000

    IP Variables and Conf Files for vlan

--- unique-diyhue/exports.sh
+++ unique-diyhue/exports.sh
@@ -1,6 +1,31 @@
-#export diyhue_ip=sed -n '1p' diyhue.conf
-#export diyhue_subnet=sed -n '2p' diyhue.conf
-#export diyhue_gateway=sed -n '3p' diyhue.conf
+DIYHUE_CONFIG=${UMBREL_ROOT}/data/storage/conf/diyhue.json
+if [ ! -f $DIYHUE_CONFIG_PATH ]
+then
+  #device
+  diyhue_dev=$( ip -json route | jq -r '.[] | select( .dst == "default" ).dev' )
+  #subnet
+  diyhue_sub=$( ip -json route | jq -r ' .[] | select( .dev == "ens33" and .scope == "link" ).dst ' )
+  #gateway
+  diyhue_gaw=$( ip -json route | jq -r '.[] | select( .dst == "default" ).gateway' )
+  #ip
+  diyhue_hip=$( ip -json route | jq -r ' .[] | select( .dev == "ens33" and .scope == "link" ).prefsrc ' )
+  #new ip
+  if [ $( echo $diyhue_hip | cut -d '.' -f 4 ) -gt 253 ] ; then
+    diyhue_nip=$( echo $diyhue_hip | cut -d '.' -f 1,2,3 ).253
+  else
+    diyhue_nip=$( echo $diyhue_hip | cut -d '.' -f 1,2,3 ).$(( $( echo $diyhue_hip | cut -d '.' -f 4 ) + 1 ))
+  fi
+  #Write Out
+  diyhue_json='{
+    "interface": "'$diyhue_dev'",
+    "subnet": "'$diyhue_sub'",
+    "gateway": "'$diyhue_gaw'",
+    "IP": "'$diyhue_nip'"
+  }'
+  echo $diyhue_json | jq | tee $DIYHUE_CONFIG
+fi
 
-#echo $(hostname -I | awk '{print $1}' | cut -d '.' -f 4)
-#diyhue_ip $(hostname -I | awk '{print $1}')
+export DIYHUE_INTERFACE=$( cat $DIYHUE_CONFIG | jq -r .interface )
+export DIYHUE_SUBNET=$( cat $DIYHUE_CONFIG | jq -r .subnet )
+export DIYHUE_GATEWAY=$( cat $DIYHUE_CONFIG | jq -r .gateway )
+export DIYHUE_IP=$( cat $DIYHUE_CONFIG | jq -r .IP )
