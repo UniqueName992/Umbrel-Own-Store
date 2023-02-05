85a07a51f1040be43517b5ebd4c2151112f6f0da
Your Name <you@example.com>
2023-02-05 13:47:30 +0000

    Test messy subnet

--- unique-diyhue/exports.sh
+++ unique-diyhue/exports.sh
@@ -1,2 +1,6 @@
-export EXTERNAL_IP=$(hostname -I | awk '{print $1}')
-export RM_JWT=$(cat /proc/sys/kernel/random/uuid)
+#export diyhue_ip=sed -n '1p' diyhue.conf
+#export diyhue_subnet=sed -n '2p' diyhue.conf
+#export diyhue_gateway=sed -n '3p' diyhue.conf
+
+#echo $(hostname -I | awk '{print $1}' | cut -d '.' -f 4)
+#diyhue_ip $(hostname -I | awk '{print $1}')
