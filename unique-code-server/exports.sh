1d8c83a88e099e9d02b4a64d1ee448a6f3bbae7f
Your Name <you@example.com>
2023-02-07 22:20:27 +0000

    test

--- unique-code-server/exports.sh
+++ unique-code-server/exports.sh
@@ -1,4 +1,6 @@
-echo $(pwd)
+# echo $(pwd)
+AppDir=${UMBREL_ROOT}/app-data/unique-code-server
+echo $AppDir
 # echo ${APP_DATA_DIR}
-echo ${UMBREL_ROOT}
+# echo ${UMBREL_ROOT}/
 # cp $APP_DATA_DIR/docker-compose2.yml $APP_DATA_DIR/docker-compose.yml
