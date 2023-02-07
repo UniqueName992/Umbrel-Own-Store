f235bd7cc4285f2eff1deb1eb471f38ba08374fb
Your Name <you@example.com>
2023-02-07 22:32:03 +0000

    test without cat

--- unique-code-server/exports.sh
+++ unique-code-server/exports.sh
@@ -3,5 +3,5 @@ AppDir=${UMBREL_ROOT}/app-data/unique-code-server
 # echo $AppDir
 # echo ${APP_DATA_DIR}
 # echo ${UMBREL_ROOT}/
-cat $AppDir/docker-compose2.yml
+# cat $AppDir/docker-compose2.yml
 cp $AppDir/docker-compose2.yml $AppDir/docker-compose.yml
