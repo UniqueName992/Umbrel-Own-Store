250bd30d8092e151b10ac1a5324da4be7c7a7927
Your Name <you@example.com>
2023-02-08 04:46:51 +0000

    Change donwnlaod dir into downloads

--- unique-openbooks/exports.sh
+++ unique-openbooks/exports.sh
@@ -6,3 +6,6 @@ export RM_NAME_name=$(curl -s https://randomuser.me/api/)
 export RM_NAME_first=$(echo $RM_NAME_name | jq ."results[0]".name.first | sed 's/"//g')
 export RM_NAME_last=$(echo $RM_NAME_name | jq ."results[0]".name.last | sed 's/"//g')
 export RM_NAME="$RM_NAME_first"_"$RM_NAME_last"
+
+export UMBREL_EBOOK_DOWNLOADS=${UMBREL_ROOT}/data/storage/downloads/ebooks
+mkdir -p $UMBREL_EBOOK_DOWNLOADS
