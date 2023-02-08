344b3ab39735a2112adc07b9c252244a75551682
Your Name <you@example.com>
2023-02-08 04:28:35 +0000

    Compose Mods

--- unique-openbooks/exports.sh
+++ unique-openbooks/exports.sh
@@ -1,3 +1,7 @@
+AppDir=${UMBREL_ROOT}/app-data/unique-openbooks
+ConfDir=${UMBREL_ROOT}/data/storage/unique-store/openbooks
+source $AppDir/unique-store.sh
+
 export RM_NAME_name=$(curl -s https://randomuser.me/api/)
 export RM_NAME_first=$(echo $RM_NAME_name | jq ."results[0]".name.first | sed 's/"//g')
 export RM_NAME_last=$(echo $RM_NAME_name | jq ."results[0]".name.last | sed 's/"//g')
