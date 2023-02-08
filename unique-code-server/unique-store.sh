22e05afdd7313bc267c243a406d5151b65c78c88
Your Name <you@example.com>
2023-02-08 01:40:03 +0000

    make unique-store code generic, source it

---
+++ unique-code-server/unique-store.sh
@@ -0,0 +1,28 @@
+AppDir=${UMBREL_ROOT}/app-data/unique-code-server
+ConfDir=${UMBREL_ROOT}/data/storage/unique-store/code-server
+
+# Make conf location
+mkdir -p $ConfDir
+
+# if no existing compsoe mods, copy template
+if [ ! -f $ConfDir/compose-mod.yml ]
+then
+    cp $AppDir/compose-mod.yml $ConfDir/compose-mod.yml
+fi
+
+# mergs compose mode and compose templates into a single docker-compose
+yq eval-all '. as $item ireduce ({}; . *+ $item)' $AppDir/docker-compose-template.yml $ConfDir/compose-mod.yml > $AppDir/docker-compose.yml
+
+# if no data dir, make it, copy template
+if [ ! -f $ConfDir/data ]
+then
+    cp $AppDir/data-template $ConfDir/data -r
+fi
+
+# if no symlink to data dir, make it
+if [ ! -f $AppDir/data ]
+then
+    ln -sf $ConfDir/data $AppDir/data
+fi
+
+export UNIQUE_APP_DATA_DIR=${ConfDir}/data
