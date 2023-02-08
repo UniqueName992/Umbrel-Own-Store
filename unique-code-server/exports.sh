904685fdf017a71e38c83d711f7200ff96d2e26d
Your Name <you@example.com>
2023-02-08 01:28:09 +0000

    tyos

--- unique-code-server/exports.sh
+++ unique-code-server/exports.sh
@@ -8,8 +8,10 @@ fi
 yq eval-all '. as $item ireduce ({}; . *+ $item)' $AppDir/docker-compose-template.yml $ConfDir/compose-mod.yml > $AppDir/docker-compose.yml
 export UNIQUE_APP_DATA_DIR=${ConfDir}/data
 if [ ! -f $ConfDir/data ]
+then
     cp $AppDir/data-template $ConfDir/data -r
 fi
 if [ ! -f $AppDir/data ]
+then
     ln -sf $ConfDir/data $AppDir/data
 fi
