c46e573bf6a63fc92bd0d24498a9a723a8253668
Your Name <you@example.com>
2023-02-08 23:39:50 +0000

    path

--- unique-portainerce/exports.sh
+++ unique-portainerce/exports.sh
@@ -1,3 +1,3 @@
-AppDir=${UMBREL_ROOT}/app-data/portainerce
+AppDir=${UMBREL_ROOT}/app-data/unique-portainerce
 ConfDir=${UMBREL_ROOT}/data/storage/unique-store/portainerce
 source $AppDir/unique-store.sh
