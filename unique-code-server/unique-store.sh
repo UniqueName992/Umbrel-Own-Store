09af0049486eee3a426dcda77eafca06bc027510
Your Name <you@example.com>
2023-02-08 02:16:13 +0000

    test

--- unique-code-server/unique-store.sh
+++ unique-code-server/unique-store.sh
@@ -10,18 +10,18 @@ fi
 # mergs compose mode and compose templates into a single docker-compose
 yq eval-all '. as $item ireduce ({}; . *+ $item)' $AppDir/docker-compose-template.yml $ConfDir/compose-mod.yml > $AppDir/docker-compose.yml
 
-# if no symlink to data dir, make it
-if [ ! -L $AppDir/data ]
-then
-    echo Hard Symlinking data directory
-    ln -s $ConfDir/data $AppDir/data
-fi
-
 # if no data dir, make it, copy template
 if [ ! -d $ConfDir/data ]
 then
     echo Copying data template
     cp $AppDir/data-template $ConfDir/data -r
+fi
+
+# if no symlink to data dir, make it
+if [ ! -L $AppDir/data ]
+then
+    echo Symlinking data directory
+    ln -s $ConfDir/data $AppDir/data
 fi
 
 export UNIQUE_APP_DATA_DIR=${ConfDir}/data
