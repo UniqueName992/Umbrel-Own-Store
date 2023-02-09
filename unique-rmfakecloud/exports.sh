d7893ac42eb17b57b5ff5adbbbd13deab70967a3
Your Name <you@example.com>
2023-02-09 00:47:02 +0000

    compose-mod

--- unique-rmfakecloud/exports.sh
+++ unique-rmfakecloud/exports.sh
@@ -1,2 +1,8 @@
+AppID=rmfakecloud
+AppDir=${UMBREL_ROOT}/app-data/unique-$AppID
+ConfDir=${UMBREL_ROOT}/data/storage/unique-store/$AppID
+source $AppDir/unique-store.sh
+
+
 export EXTERNAL_IP=$(hostname -I | awk '{print $1}')
 export RM_JWT=$(cat /proc/sys/kernel/random/uuid)
